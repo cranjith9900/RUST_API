3dc41a74f8634710ebb70deaac6853bf4d7d7c78
Ranjith9900 <cranjith9900@gmail.com>
2025-07-31 13:52:06 +0530

    intial router set up with only get

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, Route, Server};
+use poem::web::Path;
+
+#[handler]
+fn get_websites(Path(website_id): Path<String>) -> String {
+    format!("GET: {}", website_id)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/status/:website_id", get(get_websites)); 
+
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await
 }
